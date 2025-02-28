e8319b002756105893cea15f858b83cc1639625a
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-02-28 07:31:03 -0500

    Create ruby_knowledge.rb

---
+++ ruby_knowledge.rb
@@ -0,0 +1,21 @@
+knowledge_bases = [
+  "objects", "subjects", "verb"
+]
+
+chosen_knowledge_base = File.read("lib/data/knowledgebase/choice.txt").strip.to_i
+
+search_limit = knowledge_bases.size.to_i
+
+search_limit.times do
+  if chosen_knowledge_base > 2
+    puts "I've reached the limits of my knowledge."
+
+    abort
+  end
+
+  current_knowledge = knowledge_bases[chosen_knowledge_base]
+
+  system("swipl #{current_knowledge}.pl")
+
+  chosen_knowledge_base = chosen_knowledge_base + 1
+end
